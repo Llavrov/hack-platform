fae315d4372d5b4068fb7bc4771fd78ee9ee3504
leva <levlevlavrov@gmail.com>
2022-01-11 18:03:35 +0500

    procfile

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+'use strict'
+
 const express = require('express');
+const body = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+app.use(body.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, function () {
     console.log(`Server listening port is ${port}`);
